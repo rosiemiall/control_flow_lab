1640a81e8db97020b205369427e043376c9eafa0
Rosie Miall <rosiemiall38@gmail.com>
2023-10-05 17:16:48 +0100

    updated temperature options

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -13,14 +13,17 @@ public class WhatToWear {
 //      if sunny take sunglasses
         String result = weather.equals("sunny") ? "take sunglasses!" : "no need for sunglasses!";
 
+//      if >35 it is too hot or too cold
+        if (temperature > 35 || temperature < -5) {
+            System.out.println("It's not safe to go outside!");
 //      if rainy, snowy or <15 take a coat
-        if (weather.equals("rainy") || weather.equals("snowy") || temperature < 15){
+        } else if (weather.equals("rainy") || weather.equals("snowy") || temperature < 15){
             System.out.println("Take a coat and "+result);
 //      if sunny and >22 take suncream
         } else if (temperature > 22 && weather.equals("sunny")) {
             System.out.println("Take suncream and "+result);
         } else {
-            System.out.println("Take an extra layer and "+result);
+            System.out.println("Enjoy the weather and "+result);
         }
 
     }
